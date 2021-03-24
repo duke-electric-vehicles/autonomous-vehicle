aa8e62c8084b00be8e2da2fe0080190654d2b594
Jordan Schwartz <1jordanschwartz@gmail.com>
2021-03-23 22:00:26 -0400

    RTK Publishes to Topic

--- dev_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ dev_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -17,6 +17,11 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+from sbp.client.drivers.pyserial_driver import PySerialDriver
+from sbp.client import Handler, Framer
+from sbp.client.loggers.json_logger import JSONLogger
+from sbp.navigation import SBP_MSG_BASELINE_NED, MsgBaselineNED
+import argparse
 
 class MinimalPublisher(Node):
 
@@ -24,15 +29,35 @@ class MinimalPublisher(Node):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        self.parser = argparse.ArgumentParser(
+            description="Swift Navigation SBP Example.")
+        self.parser.add_argument(
+            "-p",
+            "--port",
+            default=['/dev/ttyACM1'],
+            nargs=1,
+            help="specify the serial port to use.")
+        self.args = self.parser.parse_args()
+        self.get_rtk_data()
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    def get_rtk_data(self):
+        data = String()
+
+        # Open a connection to Piksi using the default baud rate (1Mbaud)
+        with PySerialDriver(self.args.port[0], baud=1000000) as driver:
+            with Handler(Framer(driver.read, None, verbose=True)) as source:
+                try:
+                    for msg, metadata in source.filter(SBP_MSG_BASELINE_NED):
+                        # Print out the N, E, D coordinates of the baseline
+                        data.data = "%.4f,%.4f,%.4f" % (msg.n * 1e-3, msg.e * 1e-3, msg.d * 1e-3)
+                        self.publisher_.publish(data)
+                        self.get_logger().info('Publishing: "%s"' % data.data)
+                        self.i += 1
+
+                except KeyboardInterrupt:
+                    self.destroy_node()
+                    rclpy.shutdown()
 
 
 def main(args=None):
