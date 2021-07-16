809742fc1f65bda177d925c93eed993325427921
Jordan Schwartz <1jordanschwartz@gmail.com>
2021-07-15 23:33:59 -0400

    rename topic

--- ros_packages/gps_sim/gps_sim/simulate.py
+++ ros_packages/gps_sim/gps_sim/simulate.py
@@ -8,7 +8,7 @@ class GpsSimulator(Node):
 
     def __init__(self):
         super().__init__('GpsSimulator')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(String, 'gps_data_sim', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
