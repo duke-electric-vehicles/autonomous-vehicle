8212f234eab165c84f185f0c3a5c3dc183d1d8e0
1Jordan <1jordanschwartz@gmail.com>
2021-09-28 20:40:04 -0400

    Odroid (#9)
    * fixed rebase conflict
    
    * rename topic
    
    * working gps simulator
    
    * uros dockerfile for odroid
    
    * working

--- ros_packages/gps_sim/gps_sim/simulate.py
+++ ros_packages/gps_sim/gps_sim/simulate.py
@@ -2,23 +2,33 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from geometry_msgs.msg import Vector3
 
 
 class GpsSimulator(Node):
 
     def __init__(self):
         super().__init__('GpsSimulator')
-        self.publisher_ = self.create_publisher(String, 'gps_data_sim', 10)
-        timer_period = 0.5  # seconds
+        self.publisher_ = self.create_publisher(Vector3, 'gps_data_sim', 10)
+        timer_period = 0.05  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.f = open('/opt/ros/dev_ws/src/gps_sim/gps_sim/coordinates.txt', 'r')
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        line = self.f.readline()
+        if not line:
+            #Repeat
+            self.f.close()
+            self.f = open('/opt/ros/dev_ws/src/gps_sim/gps_sim/coordinates.txt', 'r')
+            line = self.f.readline()
+        strs = line.split(',')
+        msg = Vector3()
+        msg.z = 0.0 
+        msg.x = float(strs[0])
+        msg.y = float(strs[1])
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.get_logger().info(f"Publishing: {msg.x}, {msg.y}, {msg.z}")
 
 
 def main(args=None):
