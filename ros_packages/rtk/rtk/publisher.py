459146a211cc28306a0b72e83606b11b6adde4af
Owen Jennings <otjrocks2@gmail.com>
2022-03-01 20:01:30 -0500

    Updated publisher.py
    Logging feature added

--- ros_packages/rtk/rtk/publisher.py
+++ ros_packages/rtk/rtk/publisher.py
@@ -11,42 +11,42 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+ 
 import rclpy
 from rclpy.node import Node
-
+ 
 from std_msgs.msg import String
 from geometry_msgs.msg import Vector3
-
+ 
 from sbp.client.drivers.pyserial_driver import PySerialDriver
 from sbp.client import Handler, Framer
 from sbp.client.loggers.json_logger import JSONLogger
 from sbp.navigation import SBP_MSG_VEL_ECEF, SBP_MSG_POS_ECEF
 import argparse
-
+ 
 class RtkPublisher(Node):
     def __init__(self):
         super().__init__('rtk_publisher')
         self.pos_publisher = self.create_publisher(Vector3, 'rtk_pos', 10)
         self.vel_publisher = self.create_publisher(Vector3, 'rtk_vel', 10)
-
+ 
         self.parser = argparse.ArgumentParser(
             description="RTK Velocity Publisher")
         self.parser.add_argument(
             "-p",
             "--port",
-            default=['/dev/ttyACM1'],
+            default=['/dev/ttyACM0'],
             nargs=1,
             help="specify the serial port to use.")
         self.args = self.parser.parse_args()
         self.driver = PySerialDriver(self.args.port[0], baud=1000000)
-
+ 
         self.handler = Handler(Framer(self.driver.read, None, verbose=True))
         self.handler.add_callback(self.pos_pub, msg_type=SBP_MSG_POS_ECEF)
         self.handler.add_callback(self.vel_pub, msg_type=SBP_MSG_VEL_ECEF)
-
+ 
         self.handler.start()
-
+ 
     def pos_pub(self, msg, **metadata):
         pos = Vector3()
         pos.x = msg.x
@@ -54,8 +54,12 @@ class RtkPublisher(Node):
         pos.z = msg.z
         self.pos_publisher.publish(pos)
         dataStr = "%.4f,%.4f,%.4f" % (msg.x * 1e-3, msg.y * 1e-3, msg.z * 1e-3)
+        out_path = "coordinates.csv"
+        out_file = open(out_path, "a")
+        out_file.write(dataStr + "\n")
+        out_file.close()
         self.get_logger().info('Publishing Position: "%s"' % dataStr)
-
+ 
     def vel_pub(self, msg, **metadata):
         vel = Vector3()
         vel.x = msg.x * 1e-3
@@ -64,18 +68,18 @@ class RtkPublisher(Node):
         self.vel_publisher.publish(vel)
         dataStr = "%.4f,%.4f,%.4f" % (msg.x * 1e-3, msg.y * 1e-3, msg.z * 1e-3)
         self.get_logger().info('Publishing Velocity: "%s"' % dataStr)
-
-
+ 
+ 
 def main(args=None):
     rclpy.init(args=args)
-
+ 
     rtk_publisher = RtkPublisher()
-
+ 
     rclpy.spin(rtk_publisher)
-
+ 
     rtk_publisher.destroy_node()
     rclpy.shutdown()
-
-
+    out_file.close()
+ 
 if __name__ == '__main__':
     main()
