81924710b8e533c0284d6f2ddfaa9cf46d260d2c
Jordan Schwartz <1jordanschwartz@gmail.com>
2022-03-27 15:06:01 -0400

    cleaned up easycontroller platformio project

--- platformio_projects/EasyController2/src/main.cpp
+++ platformio_projects/EasyController2/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+
 #define THROTTLE_PIN 15       // Throttle pin
 #define THROTTLE_LOW 150      // These LOW and HIGH values are used to scale the ADC reading. More on this below
 #define THROTTLE_HIGH 710
@@ -20,18 +21,80 @@
 
 uint8_t hallToMotor[8] = {255, 255, 255, 255, 255, 255, 255, 255};
 
-/* Helper function to actually write values to transistors. For the low sides, takes a 0 or 1 for on/off
- * For high sides, takes 0-255 for PWM value
+// Forward declarations
+void identifyHalls();
+void writePWM(uint8_t motorState, uint8_t dutyCycle);
+void writePhases(uint8_t ah, uint8_t bh, uint8_t ch, uint8_t al, uint8_t bl, uint8_t cl);
+uint8_t getHalls();
+uint8_t readThrottle();
+
+void setup() {                // The setup function is called ONCE on boot-up
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWriteFast(LED_PIN, HIGH);
+
+  pinMode(AH_PIN, OUTPUT);    // Set all PWM pins as output
+  pinMode(AL_PIN, OUTPUT);
+  pinMode(BH_PIN, OUTPUT);
+  pinMode(BL_PIN, OUTPUT);
+  pinMode(CH_PIN, OUTPUT);
+  pinMode(CL_PIN, OUTPUT);
+
+  analogWriteFrequency(AH_PIN, 8000); // Set the PWM frequency. Since all pins are on the same timer, this sets PWM freq for all
+
+  pinMode(HALL_1_PIN, INPUT);         // Set the hall pins as input
+  pinMode(HALL_2_PIN, INPUT);
+  pinMode(HALL_3_PIN, INPUT);
+
+  pinMode(THROTTLE_PIN, INPUT);
+  
+  identifyHalls();                  // Uncomment this if you want the controller to auto-identify the hall states at startup!
+}
+
+void loop(uint8_t throttle) {                         // The loop function is called repeatedly, once setup() is done
+  
+  uint8_t throttle = readThrottle();  // readThrottle() is slow. So do the more important things 200 times more often
+  for(uint8_t i = 0; i < 200; i++)
+  {  
+    uint8_t hall = getHalls();              // Read from the hall sensors
+    uint8_t motorState = hallToMotor[hall]; // Convert from hall values (from 1 to 6) to motor state values (from 0 to 5) in the correct order. This line is magic
+    writePWM(motorState, throttle);         // Actually command the transistors to switch into specified sequence and PWM value
+  }
+}
+
+/* Magic function to do hall auto-identification. Moves the motor to all 6 states, then reads the hall values from each one
+ * 
+ * Note, that in order to get a clean hall reading, we actually need to commutate to half-states. So instead of going to state 3, for example
+ * we commutate to state 3.5, by rapidly switching between states 3 and 4. After waiting for a while (half a second), we read the hall value.
+ * Finally, print it
  */
 
-void writePhases(uint8_t ah, uint8_t bh, uint8_t ch, uint8_t al, uint8_t bl, uint8_t cl)
+void identifyHalls()
 {
-  analogWrite(AH_PIN, ah);
-  analogWrite(BH_PIN, bh);
-  analogWrite(CH_PIN, ch);
-  digitalWriteFast(AL_PIN, al);
-  digitalWriteFast(BL_PIN, bl);
-  digitalWriteFast(CL_PIN, cl);
+  for(uint8_t i = 0; i < 6; i++)
+  {
+    uint8_t nextState = (i + 1) % 6;        // Calculate what the next state should be. This is for switching into half-states
+    Serial.print("Going to ");
+    Serial.println(i);
+    for(uint16_t j = 0; j < 200; j++)       // For a while, repeatedly switch between states
+    {
+      delay(1);
+      writePWM(i, 20);
+      delay(1);
+      writePWM(nextState, 20);
+    }
+    hallToMotor[getHalls()] = (i + 2) % 6;  // Store the hall state - motor state correlation. Notice that +2 indicates 90 degrees ahead, as we're at half states
+  }
+  
+  writePWM(0, 0);                           // Turn phases off
+  
+  for(uint8_t i = 0; i < 8; i++)            // Print out the array
+  {
+    Serial.print(hallToMotor[i]);
+    Serial.print(", ");
+  }
+  Serial.println();
 }
 
 /* This function takes a motorState (from 0 to 5) as an input, and decides which transistors to turn on
@@ -59,6 +122,20 @@ void writePWM(uint8_t motorState, uint8_t dutyCycle)
       writePhases(0, dutyCycle, 0, 0, 0, 1);
   else                                        // All off
       writePhases(0, 0, 0, 0, 0, 0);
+}
+
+/* Helper function to actually write values to transistors. For the low sides, takes a 0 or 1 for on/off
+ * For high sides, takes 0-255 for PWM value
+ */
+
+void writePhases(uint8_t ah, uint8_t bh, uint8_t ch, uint8_t al, uint8_t bl, uint8_t cl)
+{
+  analogWrite(AH_PIN, ah);
+  analogWrite(BH_PIN, bh);
+  analogWrite(CH_PIN, ch);
+  digitalWriteFast(AL_PIN, al);
+  digitalWriteFast(BL_PIN, bl);
+  digitalWriteFast(CL_PIN, cl);
 }
 
 /* Read hall sensors WITH oversamping. This is required, as the hall sensor readings are often noisy.
@@ -91,39 +168,6 @@ uint8_t getHalls()
   return hall & 0x7;                            // Just to make sure we didn't do anything stupid, set the maximum output value to 7
 }
 
-
-
-void identifyHalls()
-{
-  for(uint8_t i = 0; i < 6; i++)
-  {
-    uint8_t nextState = (i + 1) % 6;        // Calculate what the next state should be. This is for switching into half-states
-    Serial.print("Going to ");
-    Serial.println(i);
-    for(uint16_t j = 0; j < 200; j++)       // For a while, repeatedly switch between states
-    {
-      delay(1);
-      writePWM(i, 20);
-      delay(1);
-      writePWM(nextState, 20);
-    }
-    hallToMotor[getHalls()] = (i + 2) % 6;  // Store the hall state - motor state correlation. Notice that +2 indicates 90 degrees ahead, as we're at half states
-  }
-  
-  writePWM(0, 0);                           // Turn phases off
-  
-  for(uint8_t i = 0; i < 8; i++)            // Print out the array
-  {
-    Serial.print(hallToMotor[i]);
-    Serial.print(", ");
-  }
-  Serial.println();
-}
-
-
-
-
-
 /* Read the throttle value from the ADC. Because our ADC can read from 0v-3.3v, but the throttle doesn't output over this whole range,
  * scale the throttle reading to take up the full range of 0-255
  */
@@ -142,46 +186,3 @@ uint8_t readThrottle()
   
   return adc;
 }
-
-void setup() {                // The setup function is called ONCE on boot-up
-  Serial.begin(115200);
-
-  pinMode(LED_PIN, OUTPUT);
-  digitalWriteFast(LED_PIN, HIGH);
-
-  pinMode(AH_PIN, OUTPUT);    // Set all PWM pins as output
-  pinMode(AL_PIN, OUTPUT);
-  pinMode(BH_PIN, OUTPUT);
-  pinMode(BL_PIN, OUTPUT);
-  pinMode(CH_PIN, OUTPUT);
-  pinMode(CL_PIN, OUTPUT);
-
-  analogWriteFrequency(AH_PIN, 8000); // Set the PWM frequency. Since all pins are on the same timer, this sets PWM freq for all
-
-  pinMode(HALL_1_PIN, INPUT);         // Set the hall pins as input
-  pinMode(HALL_2_PIN, INPUT);
-  pinMode(HALL_3_PIN, INPUT);
-
-  pinMode(THROTTLE_PIN, INPUT);
-  
-  identifyHalls();                  // Uncomment this if you want the controller to auto-identify the hall states at startup!
-}
-
-void loop() {                         // The loop function is called repeatedly, once setup() is done
-  
-  uint8_t throttle = readThrottle();  // readThrottle() is slow. So do the more important things 200 times more often
-  for(uint8_t i = 0; i < 200; i++)
-  {  
-    uint8_t hall = getHalls();              // Read from the hall sensors
-    uint8_t motorState = hallToMotor[hall]; // Convert from hall values (from 1 to 6) to motor state values (from 0 to 5) in the correct order. This line is magic
-    writePWM(motorState, throttle);         // Actually command the transistors to switch into specified sequence and PWM value
-  }
-}
-
-/* Magic function to do hall auto-identification. Moves the motor to all 6 states, then reads the hall values from each one
- * 
- * Note, that in order to get a clean hall reading, we actually need to commutate to half-states. So instead of going to state 3, for example
- * we commutate to state 3.5, by rapidly switching between states 3 and 4. After waiting for a while (half a second), we read the hall value.
- * Finally, print it
- */
-
