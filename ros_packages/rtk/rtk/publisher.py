51461cce9d7bc9d2df615844e6a9c96d27e91548
Jordan Schwartz <1jordanschwartz@gmail.com>
2023-01-30 20:01:27 -0500

    working lat long alt publisher using GeoPoint message and pyproj transformation

--- ros_packages/rtk/rtk/publisher.py
+++ ros_packages/rtk/rtk/publisher.py
@@ -12,32 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pyproj
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
 from geometry_msgs.msg import Vector3
+from geographic_msgs.msg import GeoPoint
 
 from sbp.client.drivers.pyserial_driver import PySerialDriver
 from sbp.client import Handler, Framer
-from sbp.client.loggers.json_logger import JSONLogger
 from sbp.navigation import SBP_MSG_VEL_ECEF, SBP_MSG_POS_ECEF
 import argparse
 
+ecef = pyproj.Proj(proj="geocent", ellps="WGS84", datum="WGS84")
+lla = pyproj.Proj(proj="latlong", ellps="WGS84", datum="WGS84")
+
+
 class RtkPublisher(Node):
     def __init__(self):
-        super().__init__('rtk_publisher')
-        self.pos_publisher = self.create_publisher(Vector3, 'rtk_pos', 10)
-        self.vel_publisher = self.create_publisher(Vector3, 'rtk_vel', 10)
+        super().__init__("rtk_publisher")
+        self.pos_publisher = self.create_publisher(GeoPoint, "rtk_pos", 10)
+        self.vel_publisher = self.create_publisher(Vector3, "rtk_vel", 10)
 
-        self.parser = argparse.ArgumentParser(
-            description="RTK Velocity Publisher")
+        self.parser = argparse.ArgumentParser(description="RTK Velocity Publisher")
         self.parser.add_argument(
             "-p",
             "--port",
-            default=['/dev/ttyACM1'],
+            default=["/dev/ttyACM1"],
             nargs=1,
-            help="specify the serial port to use.")
+            help="specify the serial port to use.",
+        )
         self.args = self.parser.parse_args()
         self.driver = PySerialDriver(self.args.port[0], baud=1000000)
 
@@ -48,12 +52,19 @@ class RtkPublisher(Node):
         self.handler.start()
 
     def pos_pub(self, msg, **metadata):
-        pos = Vector3()
-        pos.x = msg.x
-        pos.y = msg.y
-        pos.z = msg.z
+        lon, lat, alt = pyproj.transform(ecef, lla, msg.x, msg.y, msg.z, radians=False)
+
+        pos = GeoPoint()
+        pos.latitude = lat
+        pos.longitude = lon
+        pos.altitude = alt
+
         self.pos_publisher.publish(pos)
-        dataStr = "%.4f,%.4f,%.4f" % (msg.x * 1e-3, msg.y * 1e-3, msg.z * 1e-3)
+        dataStr = "Lat: %.4f, Long: %.4f, Alt: %.4f" % (
+            pos.latitude,
+            pos.longitude,
+            pos.altitude,
+        )
         self.get_logger().info('Publishing Position: "%s"' % dataStr)
 
     def vel_pub(self, msg, **metadata):
@@ -62,7 +73,7 @@ class RtkPublisher(Node):
         vel.y = msg.y * 1e-3
         vel.z = msg.z * 1e-3
         self.vel_publisher.publish(vel)
-        dataStr = "%.4f,%.4f,%.4f" % (msg.x * 1e-3, msg.y * 1e-3, msg.z * 1e-3)
+        dataStr = "%.4f,%.4f,%.4f" % (vel.x, vel.y, vel.z)
         self.get_logger().info('Publishing Velocity: "%s"' % dataStr)
 
 
@@ -77,5 +88,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
