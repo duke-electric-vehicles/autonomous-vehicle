87d63a3546dde71fac7d1e3a190b4d137ed07c29
Darren Wu <darren.wu@duke.edu>
2023-02-02 19:58:53 -0500

    finished clock pause/resume implementation, merged jordans ecef lat/long changes

--- ros_packages/driver_ui/driver_ui/dashboard.py
+++ ros_packages/driver_ui/driver_ui/dashboard.py
@@ -41,47 +41,92 @@ class DriverUI(Node):
 
         #initialize pygame screen / get dimensions
         #self.width, self.height = pygame.display.get_surface().get_size()
-        self.width = pygame.display.Info().current_w
-        self.height = pygame.display.Info().current_h
+        #self.width = pygame.display.Info().current_w
+        #self.height = pygame.display.Info().current_h
+
+        self.width = 400
+        self.height = 400
+
         self.screen = pygame.display.set_mode((self.width, self.height))
+  
+        self.clock = pygame.time.Clock()
+        self.time_elapsed_milli = 0
+        self.clock_status = False
 
         timer_period = 1/60  # seconds per frame
 
         self.init_ui(self.height, self.width)
-
+        timer_period = 1/60  # seconds per frame
+        self.screen.fill((0, 0, 0))
         self.timer = self.create_timer(timer_period, self.update_display)
 
+
     #Main frame loop
+    def timer_callback(self) -> None:
+
+        if self.clock_status:
+            #reset screen/(surface in the future)
+            self.screen.fill((0, 0, 0))
+            
+            self.time_elapsed_milli += self.clock.get_time()
+            self.time_elapsed_seconds = (self.time_elapsed_milli / 1000)
+            self.time_elapsed_seconds = round(self.time_elapsed_seconds, 1)
+        
+            FONT = pygame.font.SysFont("Sans", 20)
+
+            message = 'Seconds since enter: ' + str(self.time_elapsed_seconds)
+            self.screen.blit(FONT.render(message, True, (120, 120, 120)), (20, 20))
+
+        return
+
     def update_display(self) -> None:
+        
+        #timer init
+        self.clock.tick(60)
+        self.timer_callback()
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                #self.init_ui(self.screen, self.height, self.width)
+
+                #pause timer
+                if event.key == pygame.K_p:
+                    self.timer_event = 'paused'
+                    self.clock_status = False
+
+                if event.key == pygame.K_o:
+                    self.timer_event = 'unpaused'
+                    self.clock_status = True
+
                 #exit condition by pressing 'esc' button
                 if event.key == pygame.K_ESCAPE:
                     print("ESC was pressed. quitting...")
-                    quit()  
+                    quit() 
+
+        pygame.display.update()
 
     #unchanged surfaces on the screen
     def init_ui(self, height, width):
         #test ui for now
-        self.screen.fill((160, 160, 160))
-        logo = pygame.image.load("/opt/ros/dev_ws/src/driver_ui/driver_ui/logo.png") 
-        self.screen.blit(logo, (875, 35))  
-        pygame.display.update()
         
+        return
 
     #need to make static background resizeable
     #dependent on self.width, self.height
+    #needs ECEF-LLA conversion factors for pos_callback
     def position_callback(self, msg):
         
-        x_pos = msg.x
-        y_pos = msg.y
-        z_pos = msg.z
+        #Currently in ECEF
+        ECEF_x_pos = msg.x
+        ECEF_y_pos = msg.y
+        ECEF_z_pos = msg.z
         
-        global position
+        global ECEF_position
 
         #tuple cur_pos
-        position = (x_pos, y_pos, z_pos)
+        ECEF_position = (ECEF_x_pos, ECEF_y_pos, ECEF_z_pos)
+        LLA_position = ecef_to_lla(ECEF_position)
+
+
 
 
         return
