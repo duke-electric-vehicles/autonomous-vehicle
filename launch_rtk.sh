0724bbbb303a9450f9d92f03c9e6391c6c0b3344
Jordan Schwartz <1jordanschwartz@gmail.com>
2023-04-03 20:24:56 -0400

    launch script for rtk pub

---
+++ launch_rtk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run -it --net=host -e DISPLAY=${DISPLAY} -v /dev:/dev -v ./.bashrc-docker:/root/bashrc \
+  -v ./ros_packages/:/opt/ros/dev_ws/src --privileged \
+  dev_image:foxy ros2 run rtk pub
