aa450b2b179877d15830d4c138aaf6d8843c95f3
Jordan Schwartz <1jordanschwartz@gmail.com>
2023-04-03 20:27:35 -0400

    edited dashboard launch script

--- launch_dev.sh
+++ launch_dev.sh
@@ -2,4 +2,4 @@
 
 docker run -it --net=host -e DISPLAY=${DISPLAY} -v /dev:/dev -v ./.bashrc-docker:/root/bashrc \
   -v ./ros_packages/:/opt/ros/dev_ws/src --privileged \
-  dev_image:foxy ros2 run driver_ui dashboard
+  dev_image:foxy ros2 run driver_ui jackson_dashboard
