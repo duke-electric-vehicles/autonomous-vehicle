82043550040d30dcdfe8671d1380b95131950625
Jordan Schwartz <1jordanschwartz@gmail.com>
2023-04-03 20:39:55 -0400

    removed relative paths

--- launch_dev.sh
+++ launch_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -it --net=host -e DISPLAY=${DISPLAY} -v /dev:/dev -v ./.bashrc-docker:/root/bashrc \
-  -v ./ros_packages/:/opt/ros/dev_ws/src --privileged \
+docker run -it --net=host -e DISPLAY=${DISPLAY} -v /dev:/dev -v /$(pwd)/.bashrc-docker:/root/bashrc \
+  -v /$(pwd)/ros_packages/:/opt/ros/dev_ws/src --privileged \
   dev_image:foxy ros2 run driver_ui jackson_dashboard
