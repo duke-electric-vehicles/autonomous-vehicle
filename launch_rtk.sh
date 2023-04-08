c18f5422d3e962d12afa6f30e39480ad51625393
Jordan Schwartz <1jordanschwartz@gmail.com>
2023-04-08 14:31:02 -0400

    updated restart policy on launch scripts

--- launch_rtk.sh
+++ launch_rtk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -it --net=host -e DISPLAY=${DISPLAY} -v /dev:/dev -v /$(pwd)/.bashrc-docker:/root/bashrc \
+docker run -it --net=host --restart always -e DISPLAY=${DISPLAY} -v /dev:/dev -v /$(pwd)/.bashrc-docker:/root/bashrc \
   -v /$(pwd)/ros_packages/:/opt/ros/dev_ws/src --privileged \
   dev_image:foxy ros2 run rtk pub
--- launch_uros.sh
+++ launch_uros.sh
@@ -1,3 +1,3 @@
-docker run -it --net=host -v /dev:/dev --privileged \
+docker run -it --net=host --restart always -v /dev:/dev --privileged \
   microros/micro-ros-agent:foxy serial --dev \
   /dev/serial/by-id/usb-Teensyduino_USB_Serial_5886130-if00
